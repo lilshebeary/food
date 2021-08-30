3181f79fb02d4e1a8b6072d50f996ecf57749979
Ciri Ryan <cirijaye@yahoo.com>
2021-08-30 06:38:15 -0700

    add marginBottom to search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -26,7 +26,8 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         marginHorizontal: 15,
         marginTop: 10,
-        flexDirection: 'row' 
+        flexDirection: 'row',
+        marginBottom: 10 
     },
 
     inputStyle: {
