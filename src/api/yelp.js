58d537397aad7995229663e8cbfa4a9e1ffb84a5
Ciri Ryan <cirijaye@yahoo.com>
2022-02-06 09:18:16 -0800

    hide api key

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer LX2G-euPsNkrCGBcS_UmMA0_eJDUFpimdrNNvar4Pjrn5_ud4uUJwBPdqp2FIoemObIdS2vLd4R0bDXvc-vwtgxsakP8FBddKITzM3L7UdjyXR7nhBFOY_ZXswbeX3Yx'
+        Authorization: 'Bearer '
     }
 });
